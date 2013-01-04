c12db0c5ba5ff3a964f04a8611c077a6c7c88a74
ymotongpoo <ymotongpoo AT gmail.com>
2013-01-04 18:15:05 +0900

    fixed version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = ['Sphinx>=1.0']
 
 setup(
     name='sphinxcontrib-golangdomain',
-    version='0.1.1',
+    version='0.1.2',
     url='http://bitbucket.org/ymotongpoo/sphinxcontrib-golangdomain',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-golangdomain',
     license='BSD',
